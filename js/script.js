6728397f93ba12f3c81884b51aa03d8a6cd82399
billameen <bilal.ameen@icloud.com>
2025-07-03 15:07:41 -0400

    Implemented scripts to create and populate project cards

--- js/script.js
+++ js/script.js
@@ -1,4 +1,28 @@
-let croc = document.getElementById("croc");
 
-let i = 0;
+console.log("hi");
 
+const projectTemplate = document.getElementById("project-tile-template");
+const projectList = document.getElementById("project-list");
+
+fetch("../data/projects.json").then( data => {
+    data.json().then( projectData => {
+        projectData.projects.forEach( (project) => {
+            console.log(project);
+            
+            const projectTile = projectTemplate.content.cloneNode(true);
+            projectTile.getElementById("project-title").textContent = project.name;
+            projectTile.getElementById("project-desc").textContent = project.description;
+            
+            const techList = projectTile.getElementById("tech-list");
+            project.tech.forEach( e => {
+                const item = document.createElement("li");
+                item.textContent = e;
+                techList.appendChild(item);
+
+            });
+
+            projectList.append(projectTile);
+
+        });
+    });
+});
