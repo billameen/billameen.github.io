5e486c1f7bcee30e6677027328b112bf16988ce6
billameen <bilal.ameen@icloud.com>
2025-07-17 22:53:25 -0400

    Styled and implemented experience content into webpage

--- js/script.js
+++ js/script.js
@@ -27,24 +27,29 @@ fetch("../data/projects.json").then( data => {
     });
 });
 
+
+
+/*******************************/
+/** Load Experience from JSON **/
+/*******************************/
+const expTemplate = document.getElementById("exp-tile-template");
+const expList = document.getElementById("exp-list");
+
 fetch("../data/experience.json").then( data => {
     data.json().then( experienceData => {
         experienceData.experiences.forEach( (experience) => {
             
-            const projectTile = projectTemplate.content.cloneNode(true);
-            projectTile.getElementById("project-title").textContent = project.name;
-            // projectTile.getElementById("project-overview").textContent = project.overview;
-            projectTile.getElementById("project-desc").textContent = project.description;
-            
-            const techList = projectTile.getElementById("tech-list");
-            project.tech.forEach( e => {
+            const expTile = expTemplate.content.cloneNode(true);
+            expTile.getElementById("exp-title").textContent = experience.title;
+
+            const expDesc = expTile.getElementById("exp-desc-list");
+            experience.description.forEach( e => {
                 const item = document.createElement("li");
                 item.textContent = e;
-                techList.appendChild(item);
-
+                expDesc.appendChild(item);
             });
 
-            projectList.append(projectTile);
+            expList.append(expTile);
 
         });
     });
